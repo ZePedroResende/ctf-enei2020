7258ce4fd4d85a51ac5d119f098971159ef14df0
ZePedroResende <j.p.resende68@gmail.com>
2020-02-12 22:17:40 +0000

    🍺

---
+++ 2.Reverse/src/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+)
+
+func main() {
+
+	fmt.Print("Enter Password: ")
+	user_input, _, err := bufio.NewReader(os.Stdin).ReadLine()
+
+	if err != nil {
+		fmt.Println("Something is wrong with your computer, ", err)
+	}
+
+	if string(user_input) == "The answer to life is beer" {
+		fmt.Println(a)
+	} else {
+		fmt.Println("That's all folks !")
+	}
+}

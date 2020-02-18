ff8c6c3d96f0b1f38fc04d944176c8b2df16eb79
ZePedroResende <j.p.resende68@gmail.com>
2020-02-18 00:35:32 +0000

    Fix 1 flag

--- 1.Flag/app.go
+++ 1.Flag/app.go
@@ -11,7 +11,7 @@ func main() {
 	http.Handle("/Zmlub3M=", http.HandlerFunc(downloadHandler))
 
 	log.Println("Listening...")
-	http.ListenAndServe(":3000", nil)
+	http.ListenAndServe(":3001", nil)
 }
 
 func downloadHandler(w http.ResponseWriter, r *http.Request) {

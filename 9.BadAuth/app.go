3cea148748afce6f40421d27913a3a58b7fc4f28
ZePedroResende <j.p.resende68@gmail.com>
2020-02-21 00:38:18 +0000

    Add bad auth

---
+++ 9.BadAuth/app.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	// "Signin" and "Welcome" are the handlers that we will implement
+	http.HandleFunc("/signin", Signin)
+	http.HandleFunc("/welcome", Welcome)
+	http.HandleFunc("/refresh", Refresh)
+
+	// start the server on port 8000
+	log.Fatal(http.ListenAndServe(":8000", nil))
---
+++ 9.BadAuth/go.mod
@@ -0,0 +1,5 @@
+module github.com/ZePedroResende/ctf-enei2020/9.BadAuth/v2
+
+go 1.13
+
+require github.com/dgrijalva/jwt-go v3.2.0+incompatible
